99a7b57f58f1893524966e69608afd42185f476d
stephenrains <applerains1234@gmail.com>
2026-07-16 11:31:55 -0700

    New favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-          <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
-          <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-          <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png?v=2" sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg?v=2" />
+          <link rel="shortcut icon" href="/favicon/favicon.ico?v=2" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png?v=2" />
+          <link rel="manifest" href="/favicon/site.webmanifest?v=2" />
         <meta name="theme-color" content="#0b1a2e" />
       </Head>
       <body className="antialiased">
